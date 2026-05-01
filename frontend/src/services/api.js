2766e3eeabe533b5b2035a0de304f254f9d5c337
Roge-M4rc <mayanga159@outlook.es>
2026-05-01 12:44:02 -0500

    feat: conectando frontend con backend en render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://telas-system.onrender.com',
 });
 
 // 1. INTERCEPTOR DE REQUEST: Añade el Token a cada petición
